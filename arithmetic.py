bfe33b3175620475032a42b5e06b06a474d59a3c
mangalyadav1453 <136459774+mangalyadav1453@users.noreply.github.com>
2023-06-16 17:56:52 +0530

    arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,90 @@
+#arithmetic operator
+a=7
+b=2
+#addition
+print("sum:",a+b)
+#subtraction
+print("subtraction:",a-b)
+#multiplication
+print("multiplication:",a*b)
+#division
+print("division:",a/b)
+#floor division
+print("floor division:",a//b)
+#exponentation
+print("exponentation:",a**b)
+#modulo
+print("module:",a%b)
+
+#assignment operator
+a+=-2
+print("+=",a)
+a-=10
+print("-=",a)
+a=5
+a*=5
+print("*=",a)
+a=5
+a/=2
+print("/=",a)
+a=5
+a//=3
+print("//=",a)
+a=5
+a%=7
+print("%=",a)
+a=5
+a**2
+print("**=",a)
+
+#comparison operator/ relational operator
+a=5
+b=2
+#equal to operator
+print("==",a==b)
+#not equal to operator
+print('!=',a!=b)
+#less than operator
+print("<",a<b)
+#greator than operator
+print(">",a>b)
+#less than equal to operator
+print("<=",a<=b)
+#greator than equal to operator
+print(">=",a>=b)
+
+#logical operator
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+#bitwise operator
+a=10
+b=5
+#bitwise and
+print("bitwise &",a&b)
+#bitwise or
+print("bitwise |",a|b)
+#bitwise xor
+print("bitwise ^",a^b)
+#bitwise right shift
+print("bitwise >>",a>>b)
+#bitwise left operator
+print("bitwise <<",a<<b)
+
+#identity operator
+#is
+a=10
+b=10
+print("is",a is b)
+#is not
+a=5
+b=10
+print("is not", a is not b)
+
+#membership operator
+a="mangal"
+print('N' in a)
+print("y" not in a)
