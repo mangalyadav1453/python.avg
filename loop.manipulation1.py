6e7ec67bd5b79081586175fb8a24f29790fd2053
mangalyadav1453 <136459774+mangalyadav1453@users.noreply.github.com>
2023-06-18 20:11:35 +0530

    loop.manipulation1.py

---
+++ loop.manipulation1.py
@@ -0,0 +1,35 @@
+# var="a"
+# print(ord(var))
+# print(chr(97))
+
+# for i in range(0,11):
+#     if i%2!=0:
+#         print(i)
+#     else:
+#         print("even")
+
+# i=0
+# while i<=10:
+#     if i%2==0:
+#         print(i)
+#     else:
+#         print()
+#     i+=1
+
+# i=True
+# while i==True:
+#     print(i)
+#     i=False
+
+# for i in range(0,10):
+#     print(i)
+# else:
+#     print("There's no break")
+
+i=0
+while i<=5:
+    print(i)
+    i+=1
+else:
+    print("There's no break")
+
