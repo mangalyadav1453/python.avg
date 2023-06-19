199719014e3ab48dcf09922589a6d47179a63017
mangalyadav1453 <136459774+mangalyadav1453@users.noreply.github.com>
2023-06-19 19:26:32 +0530

    Update python.py

--- python.py
+++ python.py
@@ -4,5 +4,5 @@ n3=int(input("enter number in n3: "))
 print("value of n1 is : ",n1)
 print("value of n2 is : ",n2)
 print("value of n3 is : ",n3)
-avg=(n1+n2+n3)\3
+avg=(n1+n2+n3)/3
 print(" average of given number is ",avg)
